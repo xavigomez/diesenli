a8afbba456c83cb5bbd6714e8b9c480da2adefb9
Xavier Gomez <gomez.xavier@icloud.com>
2016-05-22 16:31:44 +0200

    Implemented related words filter

--- src/main/webapp/app/buscador/buscador.controller.js
+++ src/main/webapp/app/buscador/buscador.controller.js
@@ -13,17 +13,19 @@
         var vm = this;
 
         vm.searchBy = 1;
+
         vm.withSynonyms = true;
         vm.withAntonyms = true;
         vm.withEtymology = true;
-        vm.relatedMottos = 1;
+        vm.relatedMottos = true;
         vm.withExamples = true;
         vm.orderedBy = true;
+        vm.withRelateds = true;
 
+        vm.relateds = true;
+        vm.oposition = true;
+        vm.similarity = true;
 
-        vm.searchByControl = function() {
-            console.log('click');
-        }
 
         vm.filterEtimology = function(){
                 vm.withEtymology = !vm.withEtymology;
@@ -39,6 +41,44 @@
 
         vm.filterExamples = function(){
                 vm.withExamples = !vm.withExamples;
+        }
+
+        vm.filterRelateds = function(option){
+
+
+            console.log(option);
+
+            var relatedMottos = parseInt(option);
+
+            switch(relatedMottos){
+                case 1: $('#related-mottos').html('Con lemas relacionados');
+                    vm.relateds = true;
+                    vm.oposition = true;
+                    vm.similarity = true;
+                    console.log('vm.relateds = ' + vm.relateds);
+                    break;
+                case 2:  $('#related-mottos').html('Sin lemas relacionados');
+                    vm.relateds = false;
+                    vm.oposition = true;
+                    vm.similarity = true;
+                    console.log('vm.relateds = ' + vm.relateds);
+                    break;
+                case 3:  $('#related-mottos').html('Lemas por semejanza');
+                    (!vm.relateds)? vm.relateds = true:false;
+                    vm.oposition = false;
+                    vm.similarity = true;
+                    console.log('vm.similarity = ' + vm.similarity);
+                    console.log('vm.oposition = ' + vm.oposition);
+                    break;
+                case 4:  $('#related-mottos').html('Lemas por oposición');
+                    (!vm.relateds)? vm.relateds = true:false;
+                    vm.oposition = true;
+                    vm.similarity = false;
+                    console.log('vm.similarity = ' + vm.similarity);
+                    console.log('vm.oposition = ' + vm.oposition);
+                    break;
+                default: console.log('order-by error');
+            }
         }
 
         function isSearchMade(){
@@ -90,6 +130,52 @@
 
         $('span.sub-dropdown-options-container').hover(function(){
             $('.sub-dropdown-options-container', this).toggleClass('hidden-dd');
+        });
+
+        //recogemos el tipo de búsqueda
+        $('.search-by-option').click(function(){
+            $('span#search-by').data('search-by', $(this).data('search-by').toString());
+
+            var searchBy = parseInt($('#search-by').data('search-by'));
+
+            switch(searchBy){
+                case 1:
+                    $('#search-by').html('Por forma');
+                    break;
+                case 2: $('#search-by').html('Por palabra');
+                    break;
+                case 3: $('#search-by').html('Lema exacto');
+                    break;
+                case 4: $('#search-by').html('Empieza por');
+                    break;
+                case 5: $('#search-by').html('Acaba en');
+                    break;
+                case 6: $('#search-by').html('Por lema exacto');
+                    break;
+                case 7: $('#search-by').html('No empieza por');
+                    break;
+                case 8: $('#search-by').html('No acaba en');
+                    break;
+                case 9: $('#search-by').html('No contiene');
+                    break;
+                default: console.log('search-by error');
+            }
+
+        });
+
+        //Recogemos el data del tipo de ordenación
+        $('.order-by-option').click(function(){
+            $('span#order-by').data('order-by', $(this).data('order-by').toString());
+
+            var orderBy = parseInt($('#order-by').data('order-by'));
+
+            switch(orderBy){
+                case 1: $('#order-by').html('Ordenación por popularidad');
+                    break;
+                case 2: $('#order-by').html('Ordenación normal');
+                    break;
+            }
+
         });
 
     }
